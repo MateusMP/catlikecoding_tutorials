553171d1665ec991c55b548c26ec642cc0923477
MateusMP <mateus.mp93@gmail.com>
2018-05-16 09:38:16 -0300

    Add FPS counter UI

---
+++ Assets/clc_FPS/FPSDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(FPSCounter))]
+public class FPSDisplay : MonoBehaviour
+{
+    public Text fpsLabel;
+
+    FPSCounter fpsCounter;
+
+    void Awake()
+    {
+        fpsCounter = GetComponent<FPSCounter>();
+    }
+
+    void Update()
+    {
+        fpsLabel.text = fpsCounter.FPS.ToString();
+    }
+}
