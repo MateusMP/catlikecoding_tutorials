8f9d37d147dd3bdb3641210e1a9fdfba181b9213
MateusMP <mateus.mp93@gmail.com>
2018-05-18 22:12:33 -0300

    Walk along spline

---
+++ Assets/clc_Splines/Walker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+
+public class Walker : MonoBehaviour
+{
+    public BezierSpline spline;
+
+    public float duration;
+
+    private float progress;
+
+    private void Update()
+    {
+        progress += Time.deltaTime / duration;
+        if (progress > 1f)
+        {
+            progress = 0;
+        }
+        Vector3 position = spline.GetPoint(progress);
+        transform.localPosition = position;
+        transform.LookAt(position + spline.GetDirection(progress));
+    }
+}
